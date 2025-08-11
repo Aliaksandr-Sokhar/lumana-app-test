db7ce5515f134132c87b8aea5ef57331808de691
Aliaksandr Sokhar <cooldeveloper1111@gmail.com>
2025-08-11 13:21:20 +0200

    update: update config.json

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,31 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { appReducer } from './store/characters.reducers';
+import { provideEffects } from '@ngrx/effects';
+import { StoreEffects } from './store/characters.effects';
+import { MatNativeDateModule } from "@angular/material/core";
+import {
+  BrowserAnimationsModule,
+  provideAnimations,
+} from "@angular/platform-browser/animations";
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideHttpClient(),
+    provideStore({
+      characters: appReducer
+    }),
+    provideEffects([StoreEffects]),
+    provideAnimations(),
+    provideStoreDevtools({ maxAge: 30, logOnly: !isDevMode() }),
+    importProvidersFrom(MatNativeDateModule, BrowserAnimationsModule),
+  ]
 };
