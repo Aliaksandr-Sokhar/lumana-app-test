8f56705c815a6bf6dfcc9fc673cec983ddd92daf
Aliaksandr Sokhar <cooldeveloper1111@gmail.com>
2025-08-11 13:05:30 +0200

    feat: add characters-view component and base routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CharactersViewComponent } from './pages/characters-view/characters-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'all-characters',
+        pathMatch: 'full'
+    },
+    {
+        path: 'all-characters',
+        loadComponent: () => CharactersViewComponent
+    }
+];
---
+++ src/app/pages/characters-view/characters-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-characters-view',
+  imports: [],
+  templateUrl: './characters-view.component.html',
+  styleUrl: './characters-view.component.scss'
+})
+export class CharactersViewComponent {
+
+}
